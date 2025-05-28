f50946933c8fbda3b368add1e638aa746ed733fb
Bhupas Gautam <bhup0004@gmail.com>
2025-05-28 20:33:24 +0000

    database changes

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -7,6 +7,7 @@
   packages = [
     pkgs.nodejs_20
     pkgs.zulu
+    pkgs.supabase-cli
   ];
   # Sets environment variables in the workspace
   env = {};
--- src/types/supabase.ts
+++ src/types/supabase.ts
@@ -1513,6 +1513,36 @@ export type Database = {
           },
         ]
       }
+      profiles: {
+        Row: {
+          avatar_url: string | null
+          created_at: string
+          full_name: string | null
+          id: string
+          role: string
+          updated_at: string
+          user_id: string
+        }
+        Insert: {
+          avatar_url?: string | null
+          created_at?: string
+          full_name?: string | null
+          id: string
+          role?: string
+          updated_at?: string
+          user_id: string
+        }
+        Update: {
+          avatar_url?: string | null
+          created_at?: string
+          full_name?: string | null
+          id?: string
+          role?: string
+          updated_at?: string
+          user_id?: string
+        }
+        Relationships: []
+      }
       promises: {
         Row: {
           created_at: string | null
